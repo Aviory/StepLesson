81da41655b3c5cede31d6c44fb858f013ae793ca
Nobady <Nobady@169.254.158.150>
2015-02-09 11:04:26 +0200

    One

---
+++ Step/src/ConditionalStatements/One.java
@@ -0,0 +1,14 @@
+package ConditionalStatements;
+
+public class One {
+
+	public static void main(String[] args) {
+		int a=2,b=5,c;
+		if (a%2==0)
+			c=a*b;
+		else
+			c=a+b;
+		System.out.println(c);
+	}
+
+}
